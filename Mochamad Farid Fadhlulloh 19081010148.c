05047606a210e2613240f7d75a15d9beff085962
Redd <fadhlullahfarid@gmail.com>
2020-09-22 21:10:22 +0700

    first commit

---
+++ Mochamad Farid Fadhlulloh 19081010148.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("\tBIODATA \n\n");
+    printf("Nama : Mochamad Farid Fadhlulloh\n");
+    printf("TTL : Surabaya 14 September 2001\n");
+	printf("Jenis Kelamin : Laki Laki\n");
+	printf("Alamat : Sidotopo 4 115A\n");
+	printf("NPM : 19081010148\n");
+    printf("Agama : Islam\n");
+    printf("Prodi : Teknik Informatika\n");
+    
+return 0;
+}
